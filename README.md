e2bee7a79eadded859d44f9637765307a176209a
Ramon Figueiredo Pessoa <ramon@ordigraphe.com>
2019-08-06 13:27:55 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# flask_todo_app_simple_rest_api
-TODO list application: Simple REST api
+# TODO list application - Simple REST api
+
+## Table of contents
+
+- [About](#about)
+- [How to run](#how-to-run)
+
+## About
+
+API to a TODO List application.
+
+In place of a database we will store our task list in a memory structure. This will only work when the web server that runs our application is single process and single threaded. This is OK for Flask's own development web server. It is not OK to use this technique on a production web server, for that a proper database setup must be used.
+
+Each entry in the array has the following fields:
+
+	id: 			unique identifier for tasks. Numeric type.
+	title: 			short task description. String type.
+	description: 	long task description. Text type.
+	done: 			task completion state. Boolean type.
+
+The tasks resource use HTTP methods as follows:
+
+| HTTP method |	URI | Example | Action |
+| ----------- | --- | ------- | ------ |
+| **GET** | http://[hostname]/todo/api/v1.0/task | curl -i http://localhost:5000/todo/api/v1.0/tasks | Retrieve list of tasks |
+| **GET** | http://[hostname]/todo/api/v1.0/tasks/[task_id] | curl -i http://localhost:5000/todo/api/v1.0/tasks/1 | Retrieve a task |
+| **POST** | http://[hostname]/todo/api/v1.0/tasks | curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks | Create a new task |
+| **PUT** | http://[hostname]/todo/api/v1.0/tasks/[task_id] | [http://localhost:5000/todo/api/v1.0/tasks](http://localhost:5000/todo/api/v1.0/tasks) | Update an existing task |
+| **DELETE** | http://[hostname]/todo/api/v1.0/tasks/[task_id] | curl -X DELETE http://localhost:5000/todo/api/v1.0/tasks/2 | Delete a task |
+
+## How to run
+
+1. Install [virtualenv](https://virtualenv.pypa.io/en/latest/) and [Flask](https://palletsprojects.com/p/flask/)
+	* To activate the virtualenv on Linux or MacOS: ```source venv/bin/activate```
+	* To activate the virtualenv on Windows: ```\venv\Script\activate.bat```
+
+2. Run the application
+
+Step-by-step:
+
+```sh
+cd flask_todo_app_simple_rest_api/
+
+virtualenv venv
+
+source venv/bin/activate
+
+pip install flask
+
+python app.py
+```
+
---
+++ app.py
@@ -0,0 +1,222 @@
+from flask import Flask, jsonify, abort, make_response, request
+
+
+app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruits, Meat', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+'''
+GET: Retrieve list of tasks
+
+
+To test: 
+
+curl -i http://localhost:5000/todo/api/v1.0/tasks
+
+OUTPUT:
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 315
+Server: Werkzeug/0.15.5 Python/3.7.4
+Date: Tue, 06 Aug 2019 15:36:15 GMT
+
+{
+  "tasks": [
+    {
+      "description": "Milk, Cheese, Pizza, Fruits, Meat", 
+      "done": false, 
+      "id": 1, 
+      "title": "Buy groceries"
+    }, 
+    {
+      "description": "Need to find a good Python tutorial on the web", 
+      "done": false, 
+      "id": 2, 
+      "title": "Learn Python"
+    }
+  ]
+}
+'''
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+	return jsonify({'tasks': tasks})
+
+
+'''
+GET: Retrieve a task by id 
+
+
+To test:
+
+curl -i http://localhost:5000/todo/api/v1.0/tasks/1
+
+OUTPUT:
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 140
+Server: Werkzeug/0.15.5 Python/3.7.4
+Date: Tue, 06 Aug 2019 15:36:54 GMT
+
+{
+  "task": {
+    "description": "Milk, Cheese, Pizza, Fruits, Meat", 
+    "done": false, 
+    "id": 1, 
+    "title": "Buy groceries"
+  }
+}
+
+curl -i http://localhost:5000/todo/api/v1.0/tasks/3
+
+OUTPUT:
+
+HTTP/1.0 404 NOT FOUND
+Content-Type: application/json
+Content-Length: 27
+Server: Werkzeug/0.15.5 Python/3.7.4
+Date: Tue, 06 Aug 2019 15:37:19 GMT
+
+{
+  "error": "Not found"
+}
+'''
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	return jsonify({'task': task[0]})
+
+'''
+Return the 404 error in json format
+'''
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)	
+
+
+'''
+POST: Create a new task
+
+
+To test: 
+
+curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
+
+OUTPUT:
+
+HTTP/1.0 201 CREATED
+Content-Type: application/json
+Content-Length: 105
+Server: Werkzeug/0.15.5 Python/3.7.4
+Date: Tue, 06 Aug 2019 15:38:33 GMT
+
+{
+  "task": {
+    "description": "", 
+    "done": false, 
+    "id": 3, 
+    "title": "Read a book"
+  }
+}
+'''
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	task = {
+		'id': tasks[-1]['id'] + 1,
+		'title': request.json['title'],
+		'description': request.json.get('description', ""),
+		'done': False
+	}
+	tasks.append(task)
+	return jsonify({'task': task}), 201
+
+
+'''
+PUT: Update an existing task
+
+
+To test:
+
+curl -i -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/todo/api/v1.0/tasks/2
+
+OUTPUT:
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 151
+Server: Werkzeug/0.15.5 Python/3.7.4
+Date: Tue, 06 Aug 2019 15:39:07 GMT
+
+{
+  "task": {
+    "description": "Need to find a good Python tutorial on the web", 
+    "done": true, 
+    "id": 2, 
+    "title": "Learn Python"
+  }
+}
+'''
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	if not request.json:
+		abort(400)
+	if 'title' in request.json and type(request.json['title']) != unicode:
+		abort(400)
+	if 'description' in request.json and type(request.json['description']) is not unicode:
+		abort(400)
+	if 'done' in request.json and type(request.json['done']) is not bool:
+		abort(400)
+	task[0]['title'] = request.json.get('title', task[0]['title'])
+	task[0]['description'] = request.json.get('description', task[0]['description'])
+	task[0]['done'] = request.json.get('done', task[0]['done'])
+	return jsonify({'task': task[0]})
+
+
+'''
+DELETE: Delete a task
+
+
+To test:
+
+curl -X DELETE http://localhost:5000/todo/api/v1.0/tasks/2
+
+OUTPUT
+
+{
+  "result": true
+}
+'''
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+	tasks.remove(task[0])
+	return jsonify({'result': True})
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
